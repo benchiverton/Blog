1b75946f94e4351f52a3829938bc2d34a3e9b6ce
Benjamin Chiverton <b.chiverton@warwick.ac.uk>
2022-05-11 19:14:36 +0100

    Initial commit for my blog.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Blog
+
+My blog where I dump write about I find interesting.
+
+### Deployment:
+![Deploy to GihHub Pages](https://github.com/benchiverton/Blog/workflows/Deploy%20to%20GitHub%20Pages/badge.svg)
+
+The app is deployed to GitHub Pages via GitHub Actions whenever new code is pushed, following the instructions [here](https://www.davideguida.com/how-to-deploy-blazor-webassembly-on-github-pages-using-github-actions/). The latest deployment can be found here: https://benchiverton.github.io/Blog
+
+There are some issues as GitHub Pages wasn't designed to support this type of deployment, but it's mostly fine.
---
+++ src/BenChiverton.Blog/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace BenChiverton.Blog;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("app");
+
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.BrowserConsole()
+            .CreateLogger();
+
+        builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+        var host = builder.Build();
+
+        await host.RunAsync();
+    }
+}
