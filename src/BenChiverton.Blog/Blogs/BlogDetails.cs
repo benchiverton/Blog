d0a9aabedd30ec8d156237fd07ebc8bcdb78f6e8
Benjamin Chiverton <b.chiverton@warwick.ac.uk>
2022-05-13 20:20:43 +0100

    Add some boilerplate stuff for projects and blogs

---
+++ src/BenChiverton.Blog/Blogs/BlogDetails.cs
@@ -0,0 +1,8 @@
+namespace BenChiverton.Blog.Blogs
+{
+    public record BlogDetails(
+        string Name,
+        string Url,
+        string Description
+    );
+}
---
+++ src/BenChiverton.Blog/Projects/ProjectDetails.cs
@@ -0,0 +1,8 @@
+namespace BenChiverton.Blog.Projects
+{
+    public record ProjectDetails(
+        string Name,
+        string Url,
+        string Description
+    );
+}
